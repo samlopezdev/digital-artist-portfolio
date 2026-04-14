1d16cc08ffe5eb788b876c0aab57dcd3d5f259a2
SamanthaLProgramming <150631576+SamanthaLProgramming@users.noreply.github.com>
2026-04-13 18:06:27 -0700

    feat: add HireMe page with form for commission requests; update routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Commissions from './pages/Commissions';
 import About from './pages/About';
+import HireMe from './pages/HireMe'
 
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/commissions" element={<Commissions />} />
       <Route path="/about" element={<About />} />
+      <Route path="/commissions" element={<Commissions />} />
+      <Route path="/contact" element={<HireMe />} />
       <Route path="*" element={<h3>404:Page not found UwU</h3>} />
     </Routes>
   );
